959f609a2ad8853523da1de1ae6b80cfe40aec6e
Ashwani Singh <ashwani042004@gmail.com>
2025-04-05 18:48:26 +0530

    add new code

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all js code as newer version
+
+//alert(3+3) we are using nodejs not browser so it will not give the alert box
+
+console.log(3
+    +
+    3) // code readability should be high
+
+
+
+// primitive data type
+// number  let age = 21
+// bitint
+// string => ""  let name = "ashwani"
+// boolean => true/false  let isTrue = true
+// null => standalone value
+// undefined  let temp;
+// symbol => unique
+
+
+console.log(typeof null); // object
+console.log(typeof undefined)  //undefined
